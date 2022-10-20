5afccabab6470d4d444fc997eca9485c9918827b
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-20 11:22:35 +0700

    refactor the source code.

--- lib/routes/middle_ware.dart
+++ lib/routes/middle_ware.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:d3f_login/routes/links.dart';
 import 'package:fimber/fimber.dart';
 import 'package:get/get.dart';
@@ -7,20 +9,23 @@ import 'links.dart';
 class InAppPurchaseMiddleWare extends GetMiddleware {
   static observer(Routing? routing) {
     switch (routing?.current) {
-      case InAppPurchaseAppLinks.tokenIsExpired: {
-        Fimber.d('MiddleWare.observer() - AppLinks.tokenIsExpired');
-        Get.offNamed(LoginAppLinks.login);
-        break;
-      }
-      case InAppPurchaseAppLinks.splash: {
-        Fimber.d('MiddleWare.observer() - AppLinks.tokenIsExpired');
-        Get.offNamed(InAppPurchaseAppLinks.splash);
-        break;
-      }
-      default: {
-        Fimber.d('MiddleWare.observer() - unknown the app route.');
-        break;
-      }
+      case InAppPurchaseAppLinks.tokenIsExpired:
+        {
+          Fimber.d('MiddleWare.observer() - AppLinks.tokenIsExpired');
+          Get.offNamed(LoginAppLinks.login);
+          break;
+        }
+      case InAppPurchaseAppLinks.splash:
+        {
+          Fimber.d('MiddleWare.observer() - AppLinks.tokenIsExpired');
+          Get.offNamed(InAppPurchaseAppLinks.splash);
+          break;
+        }
+      default:
+        {
+          Fimber.d('MiddleWare.observer() - unknown the app route.');
+          break;
+        }
     }
   }
 }
